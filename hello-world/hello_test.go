72ccbf271dac71be0ed62082d9dc1755a2cbd0ac
4aek <4aek20@gmail.com>
2025-02-23 12:35:57 +0300

    hw-lessons-5 | feat: Hello func accept empty name argument

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when empty name argument", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
