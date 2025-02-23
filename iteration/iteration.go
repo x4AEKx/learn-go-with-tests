56b1f169daebdbfbeeccd235e403fe1ccde4782e
4aek <4aek20@gmail.com>
2025-02-23 14:39:54 +0300

    iteration-lessons-3 | feat: added count arg to Repeat func

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -2,10 +2,10 @@ package iteration
 
 import "strings"
 
-func Repeat(value string) string {
+func Repeat(value string, count int) string {
 	var repeated strings.Builder
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		repeated.WriteString(value)
 	}
 
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	expected := "aaaaa"
+	got := Repeat("a", 6)
+	expected := "aaaaaa"
 
 	if got != expected {
 		t.Errorf("expected %q but got %q", expected, got)
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
