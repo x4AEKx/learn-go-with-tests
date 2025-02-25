89cd1ccd753d74e740fe77f434fa1cd9c11d4f06
4aek <4aek20@gmail.com>
2025-02-25 20:13:16 +0300

    structs-lessons-2 | feat: added Area func

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -3,3 +3,7 @@ package structs
 func Perimeter(heigth float64, width float64) float64 {
 	return 2 * (heigth + width)
 }
+
+func Area(heigth float64, width float64) float64 {
+	return heigth * width
+}
--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -10,3 +10,12 @@ func TestPerimetr(t *testing.T) {
 		t.Errorf("expected %.2f, but got %.2f", expected, got)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	expected := 72.0
+
+	if got != expected {
+		t.Errorf("expected %.2f, but got %.2f", expected, got)
+	}
+}
