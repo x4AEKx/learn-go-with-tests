a0dfc81223e1a1670a08e56d3f390ad3da28d411
4aek <4aek20@gmail.com>
2025-02-26 23:31:54 +0300

    pointers-lessons-2 | feat: added Withdraw, Deposit methods

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,13 +3,57 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-	got := wallet.Balance()
-	expected := 10
+	assertBalace := func(t testing.TB, wallet Wallet, expected Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	if got != expected {
-		t.Errorf("expected %d, but got %d, test is %p", expected, got, &wallet.balance)
+		if got != expected {
+			t.Errorf("expected %s, but got %s, test is %p", expected, got, &wallet.balance)
+		}
 	}
+
+	assertError := func(t *testing.T, got error, want error) {
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	assertNoError := func(t *testing.T, err error) {
+		t.Helper()
+		if err != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		expected := Bitcoin(10)
+		assertBalace(t, wallet, expected)
+
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		expected := Bitcoin(10)
+
+		assertNoError(t, err)
+		assertBalace(t, wallet, expected)
+	})
+
+	t.Run("withdraw great than balance", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalace(t, wallet, startingBalance)
+
+	})
 
 }
