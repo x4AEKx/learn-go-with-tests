60608c033c5376cc2cd438a6de1770921a1cf6fa
4aek <4aek20@gmail.com>
2025-03-02 23:26:36 +0300

    maps-lessons-3 | feat: added Search method, return error

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,13 +1,27 @@
 package maps
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "testing value"}
-	got := Search(dictionary, "test")
-	expected := "testing value"
-	assertsString(t, got, expected)
+	dictionary := Dictionary{"test": "testing value"}
 
+	t.Run("known value", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		expected := "testing value"
+		assertsString(t, got, expected)
+	})
+
+	t.Run("", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		expected := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertsString(t, err.Error(), expected)
+	})
 }
 
 func assertsString(t testing.TB, got string, expected string) {
