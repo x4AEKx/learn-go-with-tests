81bfaadb57e143d4eebb0bf5e71fa082a7d97801
4aek <4aek20@gmail.com>
2025-03-03 00:00:49 +0300

    maps-lessons-4 | feat: added Add method

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,13 +2,33 @@ package maps
 
 import "errors"
 
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("value is existing")
+)
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return value, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -15,17 +15,60 @@ func TestSearch(t *testing.T) {
 
 	t.Run("", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		expected := "could not find the word you were looking for"
 
 		if err == nil {
 			t.Fatal("expected to get an error.")
 		}
-		assertsString(t, err.Error(), expected)
+		assertError(t, err, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		key := "test"
+		value := "new value"
+
+		dictionary.Add("test", "new value")
+		assertDefinition(t, dictionary, key, value)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		key := "test"
+		value := "existing"
+
+		dictionary := Dictionary{key: value}
+		err := dictionary.Add(key, "new value")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, key, value)
+	})
+
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, key string, expected string) {
+	t.Helper()
+
+	got, err := dictionary.Search(key)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertsString(t, got, expected)
+
 }
 
 func assertsString(t testing.TB, got string, expected string) {
 	t.Helper()
+	if got != expected {
+		t.Errorf("expected %q, but got %q", expected, got)
+	}
+}
+
+func assertError(t testing.TB, got, expected error) {
+	t.Helper()
+
 	if got != expected {
 		t.Errorf("expected %q, but got %q", expected, got)
 	}
